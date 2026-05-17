a08e538439aaf0296ba26e361d8053cb17c8b05c
Tanycruz93 <tania.cruz.gutierrez.al@iespoligonosur.org>
2026-05-17 13:48:41 +0200

    Añade ejercicios PHP de operadores y arrays

---
+++ Introduccion/comentario.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+
+<?php
+
+    // Soy un comentario
+
+    /*
+        Soy un comentario
+        de varias
+        líneas
+    */
+
+    # Comentario de tipo Unix
+
+    echo "Hola Mundo";
+
+?>
+
+</body>
+</html>
---
+++ Variables, arrays y operadores/destino.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+
+    Nombre : <?php echo $_GET["nombre"]; ?>
+    <br>
+
+    Cargo : <?php echo $_GET["cargo"]; ?>
+
+</body>
+</html>
